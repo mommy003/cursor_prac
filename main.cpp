193d5a3830c15d7d62f1c2c4bc3ea8c2ccf9ffb1
Md Moksedul Momin <63648257+mommy003@users.noreply.github.com>
2026-04-10 13:40:22 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -311,18 +311,17 @@ int main(int argc, const char * argv[]) {
 //                vector<McmcSamples*> mcmcSampleVec = gctb.multi_chain_mcmc(data, opt.bayesType, opt.windowWidth, opt.heritability, opt.propVarRandom, opt.pi, opt.piAlpha, opt.piBeta, opt.estimatePi, opt.pis, opt.gamma, opt.phi, opt.kappa, opt.algorithm, opt.snpFittedPerWindow, opt.varS, opt.S, opt.overdispersion, opt.estimatePS, opt.icrsq, opt.spouseCorrelation, opt.diagnosticMode, opt.robustMode, opt.numChains, opt.chainLength, opt.burnin, opt.thin, opt.outputFreq, opt.title, opt.writeBinPosterior, opt.writeTxtPosterior);
 //                if (opt.outputResults) gctb.outputResults(data, mcmcSampleVec, opt.bayesType, opt.noscale, opt.title);
 //            } else {
-                
+             
+            if (opt.analysisType == "SBayes" && opt.bayesType == "R") {
+                cout << "\nStopping before model selection/build and MCMC start for SBayesR as requested." << endl;
+                return 0;
+            }
             
             if (opt.nDistAuto) gctb.findBestFitModel(data, opt);
             
             Model *model = gctb.buildModel(data, opt, opt.bedFile, opt.gwasSummaryFile, opt.bayesType, opt.windowWidth,
                                            opt.heritability, opt.propVarRandom, opt.pi, opt.piAlpha, opt.piBeta, opt.estimatePi, opt.noscale, opt.pis, opt.piPar, opt.gamma, opt.estimateSigmaSq, opt.phi, opt.kappa,
                                            opt.algorithm, opt.snpFittedPerWindow, opt.varS, opt.S, opt.overdispersion, opt.estimatePS, opt.icrsq, opt.spouseCorrelation, opt.diagnosticMode, opt.hsqPercModel, opt.perSnpGV, opt.robustMode, opt.nDistAuto, opt.estimateRsqEnrich);
-
-            if (opt.analysisType == "SBayes" && opt.bayesType == "R") {
-                cout << "\nStopping immediately before MCMC start for SBayesR as requested." << endl;
-                return 0;
-            }
             
 //            vector<McmcSamples*> mcmcSampleVec = gctb.runMcmc(*model, opt.numChains, opt.chainLength, opt.burnin, opt.thin,
 //                                                              opt.outputFreq, opt.title, opt.writeBinPosterior, opt.writeTxtPosterior);
